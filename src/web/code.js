16886a036d6042a7475782223c43e3e82de93138
Oleh Polisan <polisanoleg@gmail.com>
2024-05-30 19:40:50 +0300

    web: add spotifyd support

--- src/web/code.js
+++ src/web/code.js
@@ -1,3 +1,44 @@
+async function parseData(spotifyData) {
+    try {
+        
+        let title = spotifyData.title;
+        let artist = spotifyData.artist;
+        let album = spotifyData.album;
+        const artURL = spotifyData.artURL;
+
+        console.log("Got title: ", title, ", artist: ", artist, ", artURL: ", artURL, ", album: ", album);
+        document.getElementById('title').innerText = title;
+        document.getElementById('album').innerText = album;
+        document.getElementById('artist').innerText = artist;
+        const albumArt = document.getElementById('album-art');
+        albumArt.style.opacity = 0;
+        const img = new Image();
+        img.onload = function() {
+            albumArt.src = artURL;
+            albumArt.style.opacity = 1;
+            document.querySelector('.background').style.backgroundImage = `url(${artURL})`;
+        };
+        img.onerror = function() {
+            console.log("New image failed to load. Keeping the old one.");
+            if (!albumArt.src) {
+                albumArt.style.opacity = 0;
+                document.querySelector('.background').style.backgroundImage = "none";
+                document.querySelector('.background').style.backgroundColor = "black";
+            }
+        };
+        if(artURL) {
+          img.src = artURL;
+        } else {
+            console.log("No art URL!");
+            albumArt.style.opacity = 0;
+            document.querySelector('.background').style.backgroundImage = "none";
+            document.querySelector('.background').style.backgroundColor = "black";
+        }
+    } catch (error) {
+        console.error('Error parsing WebSocket data', error);
+    }
+}
+
 async function getSpotifyInfoWS() {
     let socket;
 
@@ -9,45 +50,9 @@ async function getSpotifyInfoWS() {
         });
 
         socket.addEventListener('message', async function (event) {
-            try {
-                const spotifyData = JSON.parse(event.data);
-
-                let title = spotifyData.title;
-                let artist = spotifyData.artist;
-                let album = spotifyData.album;
-                const artURL = spotifyData.artURL;
-
-                console.log("Got title: ", title, ", artist: ", artist, ", artURL: ", artURL, ", album: ", album);
-                document.getElementById('title').innerText = title;
-                document.getElementById('album').innerText = album;
-                document.getElementById('artist').innerText = artist;
-                const albumArt = document.getElementById('album-art');
-                albumArt.style.opacity = 0;
-                const img = new Image();
-                img.onload = function() {
-                    albumArt.src = artURL;
-                    albumArt.style.opacity = 1;
-                    document.querySelector('.background').style.backgroundImage = `url(${artURL})`;
-                };
-                img.onerror = function() {
-                    console.log("New image failed to load. Keeping the old one.");
-                    if (!albumArt.src) {
-                        albumArt.style.opacity = 0;
-                        document.querySelector('.background').style.backgroundImage = "none";
-                        document.querySelector('.background').style.backgroundColor = "black";
-                    }
-                };
-                if(artURL) {
-                  img.src = artURL;
-                } else {
-                    console.log("No art URL!");
-                    albumArt.style.opacity = 0;
-                    document.querySelector('.background').style.backgroundImage = "none";
-                    document.querySelector('.background').style.backgroundColor = "black";
-                }
-            } catch (error) {
-                console.error('Error parsing WebSocket data', error);
-            }
+            const spotifyData = JSON.parse(event.data);
+            console.log("Got message from PC: ", spotifyData)
+            parseData(spotifyData);
         });
 
         socket.addEventListener('close', (event) => {
@@ -60,3 +65,30 @@ async function getSpotifyInfoWS() {
 
 }
 getSpotifyInfoWS()
+
+async function getSpotifyDInfoWS() {
+    let socket;
+
+    async function connectWebSocket() {
+        socket = new WebSocket(spotifydWSServer);
+
+        socket.addEventListener('open', () => {
+            console.log('WebSocket connected');
+        });
+
+        socket.addEventListener('message', async function (event) {
+            const spotifyData = JSON.parse(event.data);
+            console.log("Got message from spotifyd: ", spotifyData)
+            parseData(spotifyData);
+        });
+
+        socket.addEventListener('close', (event) => {
+            console.error('WebSocket closed. Reconnecting in 10 seconds...');
+            setTimeout(connectWebSocket, 10000); 
+        });
+    }
+
+    await connectWebSocket();
+
+}
+getSpotifyDInfoWS()
--- src/web/config.js
+++ src/web/config.js
@@ -1,1 +1,2 @@
-const PCWSServer = "ws://127.0.0.1:4831"
+const PCWSServer = "ws://192.168.0.4:4831"
+const spotifydWSServer = "ws://127.0.0.1:4831" // use "" if no spotifyd
