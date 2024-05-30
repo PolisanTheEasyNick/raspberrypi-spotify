78c4ad9559906abfeffe5cf44f3f413aa1aeedc5
Oleh Polisan <polisanoleg@gmail.com>
2024-05-30 20:51:03 +0300

    config: use own local ip

--- src/web/config.js
+++ src/web/config.js
@@ -1,2 +1,2 @@
-const PCWSServer = "ws://192.168.0.4:4831"
-const spotifydWSServer = "ws://127.0.0.1:4831" // use "" if no spotifyd
+const PCWSServer = "ws://192.168.0.4:4831";
+const spotifydWSServer = "ws://192.168.0.2:4831"; // use "" if no spotifyd
